7cdec9a27118e165c665f7a0cfc40196771411ee
nickKengne <nickdk294@gmail.com>
2024-07-27 23:33:14 +0100

    fix

--- hooks/useOnlineStatus.ts
+++ hooks/useOnlineStatus.ts
@@ -1,26 +1,25 @@
 import { useEffect, useState } from 'react';
 
-export function useOnlineStatus() : boolean {
+export function useOnlineStatus() : any {
 
-  if (typeof window == undefined) {
-    return false
+  if (typeof window != undefined) {
+    const navigatorObject = window.navigator
+    const [isOnline, setIsOnline] = useState<boolean>(navigatorObject.onLine);
+    useEffect(() => {
+      const handleOnline = () => setIsOnline(true);
+      const handleOffline = () => setIsOnline(false);
+  
+      window.addEventListener('online', handleOnline);
+      window.addEventListener('offline', handleOffline);
+  
+      return () => {
+        window.removeEventListener('online', handleOnline);
+        window.removeEventListener('offline', handleOffline);
+      };
+    }, []);
+    return isOnline
   }
+ 
 
-  const navigatorObject = window.navigator
-  const [isOnline, setIsOnline] = useState<boolean>(navigatorObject.onLine);
-
-  useEffect(() => {
-    const handleOnline = () => setIsOnline(true);
-    const handleOffline = () => setIsOnline(false);
-
-    window.addEventListener('online', handleOnline);
-    window.addEventListener('offline', handleOffline);
-
-    return () => {
-      window.removeEventListener('online', handleOnline);
-      window.removeEventListener('offline', handleOffline);
-    };
-  }, []);
-
-  return isOnline;
+  return;
 }
