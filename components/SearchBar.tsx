2eb89677bfb1164d360bf15b9fd048755580c05a
nickKengne <nickdk294@gmail.com>
2024-07-27 23:03:46 +0100

    delete somes components

--- components/SearchBar.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Input } from './ui/input'
-
-export default function SearchBar() {
-  return (
-    <Input placeholder='search candidate' type="search" className='h-9 w-[90%] relative '/>
-  )
-}
--- components/magicui/TweetCard.tsx
+++
@@ -1,9 +0,0 @@
-import ClientTweetCard from "@/app/components/magicui/tweet-card";
-
-/**
- * <TweetCard /> (Server Side Only)
- * <ClientTweetCard /> (Client Side Only)
- */
-export async function TweetCard({id}:{id:string}) {
-  return <ClientTweetCard id={id} className="shadow-2xl" />;
-}
